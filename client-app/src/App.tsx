49ff0748be434ba5fc5820bcd1beab37e93dacd0
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-06-10 23:28:19 +0800

    end of section3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,15 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
-import DuckItem from './DuckItem'
-import { ducks } from './doem'
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react'
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(response => {
+      setActivities(response.data)
+    })
+  },[])
+
   return (
-    
     <div>
-      <h1>Reactivities</h1>
-      {ducks.map(duck => (
-        <DuckItem key = {duck.name} duck={duck}/>
-      ))}
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key ={activity.id}>
+            {activity.title}
+          </List.Item>
+        ))}
+      </List>
     </div>
   )
 }
