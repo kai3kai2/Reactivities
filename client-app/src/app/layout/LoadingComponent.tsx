772e566876a0d35f9311a13813757e70e717f593
kai3kai2 <onlyyou000u@gmail.com>
2024-06-18 22:36:24 +0800

    add loding indicatosr component

---
+++ client-app/src/app/layout/LoadingComponent.tsx
@@ -0,0 +1,15 @@
+import { Dimmer, Loader } from "semantic-ui-react";
+
+
+interface Props {
+    inverted?: boolean;
+    content?: string;
+}
+
+export default function LodingComponent({inverted = true, content = 'Loading...'}: Props) {
+    return (
+        <Dimmer active={true} inverted={inverted}>
+            <Loader content={content} />
+        </Dimmer>
+    )
+}
