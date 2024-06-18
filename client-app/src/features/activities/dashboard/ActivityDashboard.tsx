4e25bbfd900f62383ce0289659d38b3752f84b69
kai3kai2 <onlyyou000u@gmail.com>
2024-06-18 23:07:57 +0800

    update date in server

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,10 +14,11 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity:Activity) => void;
   deleteActivity: (id:string) => void;
+  submitting : boolean;
 }
 
 export default function ActivityDashBoard({activities, selectedActivity, selectActivity, cancelSelectActivity
-  , editMoed, openForm, closeForm, createOrEdit, deleteActivity
+  , editMoed, openForm, closeForm, createOrEdit, deleteActivity, submitting
 }: Props)
 {
   return(
@@ -37,7 +38,12 @@ export default function ActivityDashBoard({activities, selectedActivity, selectA
         openForm={openForm}
         />}
         {editMoed &&
-        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+        <ActivityForm 
+        closeForm={closeForm} 
+        activity={selectedActivity} 
+        createOrEdit={createOrEdit}
+        submitting = {submitting}
+        />}
       </Grid.Column>
     </Grid>
   )
