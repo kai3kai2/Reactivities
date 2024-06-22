3872a6ff66a7a5b4600975f0fb5c8c2776e3e13f
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-06-22 23:27:46 +0800

    add home page& activities&createActivity router

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,11 +4,19 @@ import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useEffect } from 'react';
 
 export default observer(function ActivityDashBoard()
 {
   const {activityStore} = useStore();
   const {selectedActivity, editMode}  = activityStore;
+
+  useEffect(() => {
+    activityStore.loadActivities();
+  },[activityStore])
+
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
   return(
     <Grid>
       <Grid.Column width='10'>
