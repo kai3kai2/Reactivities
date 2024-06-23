4fff64e2a0e08dab2a95d1a0b4c5ace61be1a2b6
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-06-23 22:27:18 +0800

    clear some unuse code

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,15 +1,13 @@
-import { Button, Card, CardContent, Grid, Image } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { observer } from "mobx-react-lite";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import ActivityDetailedHeader from "./ActivityDetailedHeader";
 import ActivityDetailedInfo from "./ActivityDetailedInfo";
 import ActivityDetailedChat from "./ActivityDetailedChat";
 import ActivityDetailedSidebar from "./ActivityDetailedSidebar";
-
-
 
 export default observer(function ActivityDetails() {
   const {activityStore} = useStore();
