3c57d850518217747966a2128f80b0b8b7c8d267
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-06-23 13:34:08 +0800

    add key to the route

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -12,9 +12,9 @@ export const routes: RouteObject[] = [
     children: [
       {path: '', element: <HomePage/>},
       {path: 'activities', element: <ActivityDashboard/>},
-      {path: 'activities/:id', element: <ActivityDetails/>},
-      {path: 'createActivity', element: <ActivityForm/>},
-      {path: 'manage/:id', element: <ActivityForm/>}
+      {path: 'activities/:id', element: <ActivityDetails /> },
+      {path: 'createActivity', element: <ActivityForm key='create'/>},
+      {path: 'manage/:id', element: <ActivityForm key='manage'/>}
     ]
   },
 ]
