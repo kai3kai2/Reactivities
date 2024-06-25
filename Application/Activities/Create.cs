131f80ff902c2daf24fe2361fd6ab3e267607b9d
kai3kai2 <onlyyou000u@gmail.com>
2024-06-25 21:27:36 +0800

    add CRUD error Handling

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -7,7 +8,7 @@ namespace Application.Activities
 {
     public class Create
     {
-        public class Command : IRequest
+        public class Command : IRequest<Result<Unit>>
         {
             public Activity Activity { get; set; }
 
@@ -19,7 +20,7 @@ namespace Application.Activities
                 }
             }
 
-            public class Handler : IRequestHandler<Command>
+            public class Handler : IRequestHandler<Command, Result<Unit>>
             {
                 private readonly DataContext _context;
                 public Handler(DataContext context)
@@ -27,11 +28,15 @@ namespace Application.Activities
                     _context = context;
                 }
 
-                public async Task Handle(Command request, CancellationToken cancellationToken)
+                public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
                 {
                     _context.Activities.Add(request.Activity);
+                    
+                    var result = await _context.SaveChangesAsync() > 0;
 
-                    await _context.SaveChangesAsync();
+                    if (!result) return Result<Unit>.Failure("Failed to create activity");
+
+                    return Result<Unit>.Success(Unit.Value);
                 }
             }
         } 
