023d3e0cfa17e8cb01b28551d1a3de55d0b015b0
kai3kai2 <onlyyou000u@gmail.com>
2024-06-26 22:25:55 +0800

    add error handling on client page

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -10,7 +10,8 @@ export default function NavBar() {
         <Menu.Item as={NavLink} to='/' header>
           <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
         </Menu.Item>
-        <Menu.Item as={NavLink} to='activities' name = 'Activities'/>
+        <Menu.Item as={NavLink} to='/activities' name = 'Activities'/>
+        <Menu.Item as={NavLink} to='/errors' name = 'Errors'/>
         <Menu.Item>
           <Button as={NavLink} to='createActivity' positive content='Create Activity' />
         </Menu.Item>
--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,8 +1,10 @@
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
+import TestErrors from "../../features/errors/TestError";
+import NotFound from "../../features/errors/NotFound";
 
 export const routes: RouteObject[] = [
   {
@@ -12,7 +14,10 @@ export const routes: RouteObject[] = [
       {path: 'activities', element: <ActivityDashboard/>},
       {path: 'activities/:id', element: <ActivityDetails /> },
       {path: 'createActivity', element: <ActivityForm key='create'/>},
-      {path: 'manage/:id', element: <ActivityForm key='manage'/>}
+      {path: 'manage/:id', element: <ActivityForm key='manage'/>},
+      {path: 'errors', element: <TestErrors />},
+      {path: 'not-found', element: <NotFound />},
+      {path: '*', element: <Navigate replace to='/not-found' />}
     ]
   },
 ]
