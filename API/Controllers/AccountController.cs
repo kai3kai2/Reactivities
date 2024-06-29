c4742350be30bc694556340a944dd52fe7cfaf0d
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-06-29 18:43:43 +0800

    Creating token services

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 using API.DTOs;
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,11 @@ namespace API.Controllers
     public class AccountController : ControllerBase
     {
     private readonly UserManager<AppUser> _userManager;
-        public AccountController(UserManager<AppUser> userManager)
+    private readonly TokenService _tokenService;
+        public AccountController(UserManager<AppUser> userManager, TokenService tokenService)
         {
+            _tokenService = tokenService;
             _userManager = userManager;
-            
         }
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
@@ -30,7 +32,7 @@ namespace API.Controllers
                 {
                     DisplayName = user.DisplayName,
                     Image = null,
-                    Token = "this will be a token",
+                    Token = _tokenService.CreateToken(user),
                     Username = user.UserName
                 };
             }
