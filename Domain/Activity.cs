d5519bbf2cbada5c2309432454048f4290cb0637
kai3kai2 <onlyyou000u@gmail.com>
2024-07-02 20:03:08 +0800

    set m to m relationship

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -20,5 +20,6 @@ namespace Domain
         public string City { get; set; }
 
         public string Venue { get; set; }
+        public ICollection<ActivityAttendee> Attendees { get; set; }
     }
 }
