d06944e91ecdda5929c0922f83a36605361808f2
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-07-06 23:35:37 +0800

    update ListItem and Detail page details and button

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { format } from "date-fns";
 import ActivityListItmeAttendee from "./ActivityListItmeAttendee";
@@ -18,6 +18,21 @@ export default function ActivityListItem({activity} : Props) {
             <Item.Image size='tiny' circular src='/assets/user.png'/>
             <Item.Content>
               <Item.Header as={Link}>{activity.title}</Item.Header>
+              <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+              {activity.isHost && (
+                <Item.Description>
+                  <Label basic color='orange'>
+                    You are hosting this activity
+                  </Label>
+                </Item.Description>
+              )}
+              {activity.isGoing && !activity.isHost &&(
+                <Item.Description>
+                  <Label basic color='green'>
+                    You are going this activity
+                  </Label>
+                </Item.Description>
+              )}
             </Item.Content>
           </Item>
         </Item.Group>
