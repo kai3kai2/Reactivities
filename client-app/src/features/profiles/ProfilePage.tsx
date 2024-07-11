fb8cecd2014a1baa5d4bd0e38212eb98d71a9587
kai3kai2 <onlyyou000u@gmail.com>
2024-07-12 07:54:23 +0800

    add profile content

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,7 +1,15 @@
+import { Grid, GridColumn } from "semantic-ui-react";
+import ProfileContent from "./ProfileContent";
 import ProfileHeader from "./ProfileHeader";
 
 export default function ProfilePage() {
     return (
-        <ProfileHeader />
+        <Grid>
+            <GridColumn width={16}>
+                <ProfileHeader />
+                <ProfileContent />
+            </GridColumn>
+        </Grid>
+        
     )
 }
