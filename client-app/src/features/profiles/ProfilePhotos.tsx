357f8de5aa0dacb49bf09abf87819a9348cdb492
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-07-13 23:19:50 +0800

    add photo upload widget and use Dropzone

--- client-app/src/features/profiles/ProfilePhotos.tsx
+++ client-app/src/features/profiles/ProfilePhotos.tsx
@@ -1,22 +1,45 @@
 import { observer } from "mobx-react-lite";
-import { Card, Header, TabPane, Image } from "semantic-ui-react";
+import { Card, Header, TabPane, Image, Grid, Button } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
+import PhotoUploadWidget from "../../app/common/imageUpload/PhotoUploadWidget";
 
 interface Props {
   profile: Profile;
 }
 
 export default observer(function ProfilePhotos({profile}: Props) {
+  const {profileStore: {isCurrentUser}} = useStore();
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+
   return (
     <TabPane>
-      <Header icon='image' content='Photos'/>
-      <Card.Group itemsPerRow={5}>
-        {profile.photos?.map(photo => (
-          <Card key={photo.id}>
-            <Image src={photo.url}/>
-          </Card>
-        ))}
-      </Card.Group>
+      <Grid>
+        <Grid.Column width={16}>
+          <Header floated="left" icon='image' content='Photos'/>
+          {isCurrentUser && (
+            <Button floated='right' basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}
+            />
+          )}
+        </Grid.Column>
+        <Grid.Column width={16}>
+          {addPhotoMode ? (
+            <PhotoUploadWidget />
+          ) : (
+            <Card.Group itemsPerRow={5}>
+              {profile.photos?.map(photo => (
+                <Card key={photo.id}>
+                  <Image src={photo.url}/>
+                </Card>
+              ))}
+            </Card.Group>
+          )}
+        </Grid.Column>
+      </Grid>
+     
     </TabPane>
   )
 })
