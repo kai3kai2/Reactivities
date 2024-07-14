978cf83f07f161d63fac4e9c31857c83068a3ac2
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-07-14 11:08:00 +0800

    add react-Cropper and use it

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,10 +1,25 @@
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Button, Grid, Header } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 export default function PhotoUploadWidget() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [files, setFiles] = useState<any>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+
+  function onCrop() {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob(blob => console.log(blob));
+    }
+  }
+
+  useEffect(() => {
+    return (() => {
+      files.forEach((file: object & {preview?: string}) => URL.revokeObjectURL(file.preview!))
+    })
+  }, [files])
+
   return (
     <Grid>
       <Grid.Column width={1} />
@@ -19,7 +34,7 @@ export default function PhotoUploadWidget() {
       <Grid.Column width={4}>
         <Header sub color='teal' content='Step 2 - Resize image' />
         {files && files.length > 0 && (
-          <Image src={files[0].preview} />
+          <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
         )}
       </Grid.Column>
 
@@ -27,6 +42,14 @@ export default function PhotoUploadWidget() {
 
       <Grid.Column width={4}>
         <Header sub color='teal' content='Step 1 - Preview & Upload' />
+        {files && files.length > 0 &&
+        <>
+          <div className="img-preview" style={{minHeight: 200, overflow: 'hidden'}}/>
+          <Button.Group widths={2}>
+            <Button onClick={onCrop} positive icon='check' />
+            <Button onClick={() => setFiles([])} icon='close' />
+          </Button.Group>
+        </>}
       </Grid.Column>
     </Grid>
   )
