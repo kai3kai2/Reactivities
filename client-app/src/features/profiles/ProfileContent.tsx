409cbb9958df021096e6a742eeaa8c296c53861e
kai3kai2 <onlyyou000u@gmail.com>
2024-07-25 22:43:35 +0800

    update following & follower state and refix display error

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -2,12 +2,15 @@ import { Tab, TabPane } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import ProfilePhotos from "./ProfilePhotos";
 import ProfileFollowings from "./ProfileFollowings";
+import { useStore } from "../../app/stores/store";
 
 interface Props {
     profile: Profile;
 }
 
 export default function ProfileContent({profile}: Props) {
+    const {profileStore} = useStore();
+
     const panes = [
         {menuItem: 'About', render: () => <TabPane> About Content</TabPane>},
         {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile}></ProfilePhotos>},
@@ -21,6 +24,7 @@ export default function ProfileContent({profile}: Props) {
             menu={{fluid: true, vertical: true}}
             menuPosition="right"
             panes={panes}
+            onTabChange={(_, data) => profileStore.setActiveTab(data.activeIndex as number)}
         />
     )
 }
--- client-app/src/features/profiles/ProfileFollowings.tsx
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -1,22 +1,21 @@
 import { observer } from "mobx-react-lite"
 import { useStore } from "../../app/stores/store";
-import { Card, Grid, Header, Tab, TabPane } from "semantic-ui-react";
+import { Card, Grid, Header, TabPane } from "semantic-ui-react";
 import ProfileCard from "./ProfileCard";
-import { useEffect } from "react";
 
 export default observer(function ProfileFollowings() {
     const {profileStore} = useStore();
-    const {profile, followings, loadFollowings, loadingFollowings} = profileStore;
-
-    useEffect(() => {
-        loadFollowings('following')
-    }, [loadFollowings])
+    const {profile, followings, loadingFollowings, activeTab} = profileStore;
 
     return (
         <TabPane loading={loadingFollowings} >
             <Grid>
                 <Grid.Column width={16}>
-                    <Header floated="left" icon='user' content={`People following ${profile?.displayName}`}/>
+                    <Header 
+                        floated="left" 
+                        icon='user' 
+                        content={activeTab === 3 ? `People following ${profile?.displayName}` : `People ${profile?.displayName} is following`}
+                        />
                 </Grid.Column>
                 <Grid.Column width={16}>
                     <Card.Group itemsPerRow={4}>
