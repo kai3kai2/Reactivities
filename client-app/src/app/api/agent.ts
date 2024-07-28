57eb67a84ad77cdd9524022e0f4b925970083af5
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2024-07-28 22:08:54 +0800

    adding client side pagination and pagination parameters

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -5,6 +5,7 @@ import { router } from "../router/Routes";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
 import { Profile } from "../models/profile";
+import { PaginationResult } from "../models/pagination";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -23,7 +24,12 @@ axios.interceptors.request.use(config => {
 })
 
 axios.interceptors.response.use(async response => {
-    await sleep(1000);
+    await sleep(700);
+    const pagination = response.headers['pagination'];
+    if (pagination) {
+        response.data = new PaginationResult(response.data, JSON.parse(pagination));
+        return response as AxiosResponse<PaginationResult<unknown>>
+    }
     return response;
 }, (error: AxiosError) => {
     const {data, status, config} = error.response as AxiosResponse;
@@ -70,7 +76,8 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<Activity[]>('/activities'),
+    list: (params: URLSearchParams) => axios.get<PaginationResult<Activity[]>>('/activities', {params})
+        .then(responseBody),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
     create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
     update: (activity: ActivityFormValues) => requests.put<Activity>(`/activities/${activity.id}`, activity),
